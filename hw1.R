fec084efaff507c7ef399557ca47a1cc4d7f3ec6
skwu5678 <skwu5678@gmail.com>
2016-01-21 01:13:29 +0800

    Create hw1.R

---
+++ hw1.R
@@ -0,0 +1,12 @@
+getwd()
+setwd("C:\\20160120")
+getwd()
+df1 <- read.csv("train_users_2.csv", header=TRUE, sep=",")
+head(df1)
+tail(df1)
+df2 <- read.csv("sessions99.csv", header=TRUE, sep=",")
+install.packages("sqldf")
+library(sqldf)
+df3 <- sqldf("SELECT * FROM df1 LEFT JOIN df2 on df1.id=df2.user_id") 
+?write.csv
+write.csv(df3, file="answer2.csv")
